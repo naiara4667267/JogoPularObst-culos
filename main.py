cf6b2d8d10aea5774182987cf3f5ae4b8f9e040e
Naiara <naisales7@gmail.com>
2025-03-20 11:44:34 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+
+print("Setup Start")
+# Começa o Pygame
+pygame.init()
+window = pygame.display.set_mode(size=(600, 480))
+print("Setup End")
+
+print("Loop Start")
+
+while True:
+    for event in pygame.event.get():
+     if event.type == pygame.QUIT:
+         pygame.quit() #fecha a janela
+         quit()  #finaliza o pygame
+
+
+
+
+
